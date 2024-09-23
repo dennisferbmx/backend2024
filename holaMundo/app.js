73c09fb569392fcb9851c3b784190ddecff6bfe1
Dennis Fernando Jimenez Hernandez <dennisfer294@gmail.com>
2024-09-23 11:51:10 -0600

    agregar el arreglo tasks y filtra las tareas realizadas

--- holaMundo/app.js
+++ holaMundo/app.js
@@ -1,3 +1,20 @@
-const {otroMensaje} = require("./hola")
+const tarea =[
+    {
+        nombre: "estudiar para el examen de backend"
+        fecha: "2024-09-14"
+        hecho: false 
+    },
+    {
+        nombre: "ir de compras"
+        fecha: "2024-09-25"
+        hecho: false 
+    },
+    {
+        nombre: "hacer tareas"
+        fecha: "2024-09-22"
+        hecho: true 
+    },
+]; 
 
-console.log("Este es el punto de entrada")
+const tareasPorRealizar=tareas.filter(tarea=> !tarea.hecho);
+console.log(tareasPorRealizar)

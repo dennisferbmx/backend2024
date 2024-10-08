9749ea57841e8501ff53ff5ddc2443e839eca4e6
Dennis Fernando Jimenez Hernandez <dennisfer294@gmail.com>
2024-10-08 11:58:06 -0600

    avance de la clase

--- control_usuario/app.js
+++ control_usuario/app.js
@@ -23,11 +23,23 @@ app.get("/usuarios", (req, res)=>{
 app.get("/usuarios/:id", (req, res)=>{
 
     const {id} = req.params;
+
+    if(isNaN(id)){
+        res.status(400).send({error:"el id debe ser un numero"});
+            return;
+        };
+
+
     const usuario = usuarios.find((usuario)=> usuario.id == +id);
+
+    if(usuario == undefined){
+        res.status(404).send({error: 'el usuario con id ${id} no existe'});
+        return;
+    };
    
     res.status(200).send(usuario);
-}
-);
+});//END POINT
+
 app.listen(3000, ()=>{
     console.log("servidor corriendo en https://localhost:3000");
 });

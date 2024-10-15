1579584ab00eee8e0f44313c8c318c93740c05e0
Dennis Fernando Jimenez Hernandez <dennisfer294@gmail.com>
2024-10-15 11:59:37 -0600

    avance de la clase 4

--- control_usuario/app.js
+++ control_usuario/app.js
@@ -42,12 +42,59 @@ app.get("/usuarios/:id", (req, res)=>{
     res.status(200).send(usuario);
 });//END POINT
 
+
 app.post("/usuarios", (req, res)=>{
     const {nombre, apellido, email} = req.body;
-
+if(!nombre || !apellido || !email){
+    res.status(400).send({error: "necesitamos todo"})
+    return;
+}
+if(usuarios.find((usuario)=>usuario.email=== email)){
+    res.status(400).send({error: "El email ya existe"})
+    return;
+}
     usuarios.push({id: usuarios.length + 1, nombre, apellido, email});
 
     res.status(201).send("El usuario se agrego correctamente")
+    
+});
+
+app.put("/usuarios/:id", (req, res) =>{
+    const {nombre, apellido, email} = req.body;
+    const id = +req.params.id
+
+if(!nombre || !apellido || !email){
+    res.status(400).send({error: "necesitamos todo"})
+    return;
+}
+
+if(isNaN(id)){
+    res.status(400).send({error:"el id debe ser un numero"});
+        return;
+    };
+
+
+const usuario = usuarios.find((usuario)=> usuario.id == +id);
+
+if(usuario == undefined){
+    res.status(404).send({error: 'el usuario con id ${id} no existe'});
+    return;
+};
+
+usuarios.forEach((usuarios) => {
+    if(usuario.id === id){
+        usuario.nombre = nombre;
+        usuario.apellido = apellido;
+        usuario.email = email;
+    }
+})
+
+res.status(200).send("se actalizo bien")
+
+});
+
+app.patch("/usuarios/:id", (req, res) =>{
+    
 });
 
 app.listen(3000, ()=>{

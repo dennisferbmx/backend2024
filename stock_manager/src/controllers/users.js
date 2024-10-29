826c11b10946d5d5221d8cdff1e883e0b679e0ac
Dennis Fernando Jimenez Hernandez <dennisfer294@gmail.com>
2024-10-29 11:36:13 -0600

    clase 29/10/2024

--- stock_manager/src/controllers/users.js
+++ stock_manager/src/controllers/users.js
@@ -29,4 +29,64 @@ const getById = (req = request, res = response) =>{
 
 }
 
-module.exports = {getAll, getById};
+const postUser = (req = request, res = response) => {
+    const {name} = req.body;
+
+    if(!name){
+        res.status(400).send("bad request: the name fild is missing.");
+    }
+
+    const user = users.find(user => user.name === name);
+
+    if(user){
+        res.status(409).send("user alreay ")
+    return; 
+}
+
+users.push({id: users.length + 1, name});
+res.send("user create succesfully")
+}
+
+const updateUser = (req = request, res = response) => {
+    const {id} = req.params;
+    const {name} = req.body;
+
+    if (isNaN(id)){
+        res.status(400).send('Invalid ID');
+        return;
+    }
+
+    const user = users.find(user => user.id === +id);
+
+    if (!user){
+        res.status(404).send('user not found');
+        return;
+    }
+
+    users.forEach(user => {
+        if(user.id === +id) {
+            user.name = name;
+        }
+    });
+    res.send("user updated succerfully");
+}
+
+const deleteUser = (req = request, res = response) => {
+    const {id} = req.params;
+    if (isNaN(id)){
+        res.status(400).send('Invalid ID');
+        return;
+    }
+
+    const user = users.find(user => user.id === +id);
+
+    if (!user){
+        res.status(404).send('user not found');
+        return;
+    }
+
+    users.splice(users.findIndex ((user)=>user.id===+id),1);
+    res.send('User deleted');
+  };
+
+module.exports = {getAll, getById, postUser, updateUser, deleteUser};
--- stock_manager/src/routes/users.js
+++ stock_manager/src/routes/users.js
@@ -1,10 +1,12 @@
 const {Router} = require('express');
-const {getAll, getById} = require('../controllers/users');
+const {getAll, getById, postUser, updateUser, deleteUser} = require('../controllers/users');
 
 const router = Router();
 
 router.get('/', getAll);
-
 router.get('/:id', getById);
+router.post('/', postUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;

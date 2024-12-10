fa49765baf6e8afae5911145f833108485c7b24f
Dennis Fernando Jimenez Hernandez <dennisfer294@gmail.com>
2024-12-10 10:10:02 -0600

    07/12/2024

--- pokemon_game/src/models/pokemons.js
+++ pokemon_game/src/models/pokemons.js
@@ -1,6 +1,11 @@
 const pokemonsModel = {
     getAll: 'SELECT * FROM pokemons',
-    
+    getById: 'SELECT * FROM pokemons WHERE id = ?',
+    getExisting: 'SELECT * FROM pokemons WHERE name = ? OR image = ?',
+    addPokemon: 'INSERT INTO pokemons (name, image) VALUES (?, ?)',
+    editPokemon: 'UPDATE pokemons SET name = COALESCE(?, name), image = COALESCE(?, image) WHERE id = ?',
+    pokemonvalid: 'SELECT * FROM pokemons WHERE (name = ? OR image = ?) AND id != ?',
+    eliminarPokemon: 'DELETE FROM pokemons WHERE id = ?',
 };
 
 module.exports = pokemonsModel;
